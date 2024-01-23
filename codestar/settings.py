3e794cc96e89eec14200751788b9b2a23c007ed1
Sara Tisell <Sara.Tisell@hotmail.com>
2024-01-23 10:02:38 +0000

    update for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b7_lxg)--l(=2h(6is(u6#a)+j6c!+*p$3sf=#f0d!6m4!7zgt'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-saratisell-djangoblog-59g2f1e68vu.ws-eu107.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-saratisell-djangoblog-aigcb73u8al.ws-eu107.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
